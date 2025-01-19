ff42f990b2daeb22fba02cd109e250e1bceb0bd9
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-01-19 14:39:02 +0100

    fit: lista articoli use state

---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const list = []
+//titolo articolo e lista articoli
+const Form = () => {
+  const [title, setTitle] = useState(list);
+  const [articles, setArticles] = useState({ text: "" });
+}
+
+export default Form;
